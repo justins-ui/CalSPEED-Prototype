5149902e066714d7f648b389284962c629f05d2c
justins-ui <justinshimokochi@gmail.com>
2026-04-14 23:00:05 -0700

    Hero Section + Cards Section
    Fixed spacing issues in the hero section. Started making the cards section.

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
 const closeButton = document.querySelector('.close-button');
+const cardInfo = [
+    {
+        caption: "Test Different Devices",
+        image: "images/Devices.png"
+    },
+
+    {
+        caption: "Test From Different Locations",
+        image: "images/Locations.png"
+    },
+
+    {
+        caption: "Save Your Previous Tests",
+        image: "images/Previous.png"
+    }
+];
+const cards = document.querySelector('.cards-section');
+
+cardInfo.forEach(card => cards.innerHTML += `
+    <div class="card">
+        <img src="${card.image}">
+        <p>${card.caption}</p>    
+    </div>
+`)
 
 hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active-links');
+    navLinks.classList.add('active-links');
 })
 
 closeButton.addEventListener ('click', () => {
